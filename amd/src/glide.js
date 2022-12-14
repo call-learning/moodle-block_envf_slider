e94cdae89758afc5a9973a4ee779f53241ad8da1
martin-call-learning <martin@call-learning.fr>
2022-12-14 20:24:44 +0100

    removed slidefooter

--- amd/src/glide.js
+++ amd/src/glide.js
@@ -5,7 +5,7 @@
  * @copyright 2020 - CALL Learning - Laurent David <laurent@call-learning>
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-define(['jquery', 'block_rss_thumbnails/config'], function($) {
+define(['jquery', 'block_envf_slider/config'], function($) {
     return function(locator, config) {
         require(['glide'], function(Glide) {
             // Show the slider now we are initialised.
